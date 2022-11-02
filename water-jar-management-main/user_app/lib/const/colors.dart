84120f1f464f313c3d0a84bf3ab9aa5ba4858319
Arieyshma Chowhan <arieyshmachowhan21.com>
2022-11-03 01:22:59 +0530

    first commit

---
+++ water-jar-management-main/user_app/lib/const/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+
+Color btnColor = Color.fromARGB(99, 12, 85, 90);
+// Color bgColor = Color.fromARGB(235, 161, 241, 215);
+Color bgColor = Color.fromARGB(99, 133, 145, 143);
